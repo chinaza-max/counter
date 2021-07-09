0270d778e32d0919054944aa577e63dc364add87
unknown <mosesogbonna@gmail.com>
2021-07-09 17:10:32 -0600

    first

---
+++ script.js
@@ -0,0 +1,24 @@
+let message=document.getElementById("display")
+let count=0;
+document.getElementById("lowerContainer").addEventListener("click",lowerContainer)
+document.getElementById("addContainer").addEventListener("click",addContainer)
+function lowerContainer(){
+        
+        let num=document.getElementById('display').textContent;
+        num=parseInt(num)
+      
+        if(num>=1){
+            num--;
+            
+            message.innerHTML=num
+        }
+}
+function addContainer(){
+    let num=document.getElementById('display').textContent;
+    num=parseInt(num)
+    if(num<=14){
+        num++;
+        
+        message.innerHTML=num
+    }
+}
